742e9c362d1bafc711f85aff58aa0c7ad232458c
jomasim <simjoms@gmail.com>
2020-04-27 12:29:31 +0300

    adds react-router implementation instructions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import Todos from './components/Todos';
 
+/*
+install react-router-dom
+import react-router-dom in this file
+use Browserrouter, switch and Route to set up 
+your navigation
+ example
+ <BrowserRouter>
+    <Switch>
+      <Route path="/users" component={usersView} />
+    <Switch/>
+ </BrowserRouter>
+*/
+
 const App = () => { // es6 standard function App(){ }
   return (
     <Todos />
